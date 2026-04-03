d3cb0e5696db84a0f12f4ebd083a3834a797ee6d
athitthiyan <athitthiyananofficial@gmail.com>
2026-04-04 01:35:20 +0530

    environment changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://hotelapi-backend.onrender.com',
+  apiUrl: 'https://hotel-api-production-447d.up.railway.app/',
   paymentAppUrl: 'https://payflow-payment-app.vercel.app/',
   stripePublishableKey: 'pk_test_51TH0UuBDYx3dIveAYRnmWFuHY6EB8yZigeKqjltdRnQpc3iidRxSDV6rdmZrH8bcRt9fg3HIBCp32GRpMTnjSGcy00KPKiGzPL',
 };
